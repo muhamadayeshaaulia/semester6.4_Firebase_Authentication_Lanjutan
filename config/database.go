cd321d7a4a3f3d97bd53e37925b156e909d06d62
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-10 22:06:19 +0700

    menambahkan func untuk database host port dan lainnya

--- config/database.go
+++ config/database.go
@@ -12,3 +12,13 @@ import (
 	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
 
 )
+
+var DB *gorm.DB
+
+func initDatabase(){
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+}

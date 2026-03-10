052a454a1a0d2a503ffbb50142387e7f7e231530
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-10 22:13:59 +0700

    mebuat format dsn untuk koneksi ke mysql dan memberikan komentar sebagai penjelasan penggunaan code untuk apa

--- config/database.go
+++ config/database.go
@@ -13,12 +13,21 @@ import (
 
 )
 
+//DB instance GORM global yang di pakai di seluruh aplikasi
 var DB *gorm.DB
 
 func initDatabase(){
+	//mengambil konfigurasi database dari environment variables
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+
+	//membuat format DSN (Data Source Name) untuk koneksi ke database
+	//Format : user:pass@tcp(host:port)/dbname?params
+	dsn := fmt.Sprint(
+		"%s:%s@tcp(%s:%s)/%schartset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, dbname,
+	)
 }

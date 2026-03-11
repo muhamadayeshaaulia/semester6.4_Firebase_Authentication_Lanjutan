159ff6b7be504c7054bf6e10a7bc24641785ec75
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-11 11:40:32 +0700

    membuat filter berdasarkan kategori yang ada

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -10,3 +10,15 @@ type ProductRepository struct{}
 func NewProductRepository() *ProductRepository{
 	return &ProductRepository{}
 }
+//FindAll mengambil semua product aktif dengan pagination
+func (r *ProductRepository) FindAll (page, limit int, category string)
+([]models.Product, int64, error) {
+	var products []models.Product
+	var total int64
+
+	query := config.DB.Model(&models.Product{}). Where("is_actrive = ?", true)
+	//filter by category jika ada
+	if category != "" {
+		query = query.Whare("category = ?", category)
+	}
+}

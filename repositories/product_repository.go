0121884962efcf72f66af462a249d2bafc854c82
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-11 12:03:47 +0700

    memperbaiki credpath menghapus auth keknya saya kelebihan ngetik hehe

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -3,47 +3,50 @@ package repositories
 import (
 	"github.com/muhamadayeshaaulia/gin-firebase-backend/config"
 	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
-
 )
+
 type ProductRepository struct{}
 
-func NewProductRepository() *ProductRepository{
+func NewProductRepository() *ProductRepository {
 	return &ProductRepository{}
 }
-//FindAll mengambil semua product aktif dengan pagination
-func (r *ProductRepository) FindAll (page, limit int, category string)
-([]models.Product, int64, error) {
+
+// FindAll mengambil semua product aktif dengan pagination
+func (r *ProductRepository) FindAll(page, limit int, category string) ([]models.Product, int64, error) {
 	var products []models.Product
 	var total int64
 
-	query := config.DB.Model(&models.Product{}). Where("is_actrive = ?", true)
+	query := config.DB.Model(&models.Product{}).Where("is_active = ?", true)
 	//filter by category jika ada
 	if category != "" {
-		query = query.Whare("category = ?", category)
+		query = query.Where("category = ?", category)
 	}
 	//hitung total untuk pagination
 	query.Count(&total)
 	//ambil data dengan offset & limit
 	offset := (page - 1) * limit
 	result := query.Offset(offset).Limit(limit).Find(&products)
-	return products, total, result.error
+	return products, total, result.Error
 }
 
 // findid mengambil satu produk berdasarkan id
 func (r *ProductRepository) FindByID(id uint) (*models.Product, error) {
- var product models.Product
- result := config.DB.First(&product, id)
- return &product, result.Error
+	var product models.Product
+	result := config.DB.First(&product, id)
+	return &product, result.Error
 }
+
 // Create menyimpan produk baru
 func (r *ProductRepository) Create(product *models.Product) error {
- return config.DB.Create(product).Error
+	return config.DB.Create(product).Error
 }
+
 // Update memperbarui produk
 func (r *ProductRepository) Update(product *models.Product) error {
- return config.DB.Save(product).Error
+	return config.DB.Save(product).Error
 }
+
 // Delete soft-delete produk (tidak hapus dari DB)
 func (r *ProductRepository) Delete(id uint) error {
- return config.DB.Delete(&models.Product{}, id).Error
+	return config.DB.Delete(&models.Product{}, id).Error
 }

9dd442f0393e5bd2341454673bfbdd0a72b79435
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-11 11:48:56 +0700

    menyimpan produk baru

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -35,3 +35,7 @@ func (r *ProductRepository) FindByID(id uint) (*models.Product, error) {
  result := config.DB.First(&product, id)
  return &product, result.Error
 }
+// Create menyimpan produk baru
+func (r *ProductRepository) Create(product *models.Product) error {
+ return config.DB.Create(product).Error
+}

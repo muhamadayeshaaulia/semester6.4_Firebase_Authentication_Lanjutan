b2cd095c3d4ae29a506fb427ac3de836ac1b6268
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 02:16:10 +0700

    membuat import untuk keperluan main dan  func untuk load env dari .env

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/config"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/routes"
+)
+
+func main() {
+	//Load environment variables dari .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("File .env tidak ditemukan, menggunakan environment variable sistem")
+	}
+}

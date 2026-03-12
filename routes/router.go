5ee06b1753c11bb77f852ccbb02e286ec4ca0f6a
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 02:03:26 +0700

    memperbaiki keperluan import untuk router

--- routes/router.go
+++ routes/router.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	"net/http"
-	"strconv"
+
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
-	"github.com/muhamadayeshaaulia/gin-firebase-backend/services"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/handlers"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/middleware"
+
 )

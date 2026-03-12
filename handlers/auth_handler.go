d722e481098cc0c1c2d706153972022f70907570
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 01:47:27 +0700

    membuat untuk menerima firebase id token untuk di verivikasi dan di kembalikan ke backend jwt

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -1,13 +1,32 @@
 package handlers
-import(
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/services"
 	"net/http"
- 	"time"
- 	"github.com/gin-gonic/gin"
- 	"github.com/muhamadayeshaaulia/gin-firebase-backend/services"
+	"time"
 )
+
 type AuthHandler struct {
- authService *services.AuthService
+	authService *services.AuthService
 }
+
 func NewAuthHandler() *AuthHandler {
- return &AuthHandler{authService: services.NewAuthService()}
+	return &AuthHandler{authService: services.NewAuthService()}
+}
+
+// POST /auth/verify-token
+// Terima Firebase ID Token → verifikasi → return Backend JWT
+func (h *AuthHandler) VerifyToken(c *gin.Context) {
+	// 1. Parse request body
+	var req struct {
+		FirebaseToken string `json:"firebase_token" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "firebase_token wajib diisi",
+		})
+		return
+	}
 }

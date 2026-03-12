6fca9238d97231d8538a241b672cc1770d30d835
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 01:05:26 +0700

    mengambil token dari header authorization

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -9,3 +9,19 @@ import (
 	"github.com/gin-gonic/gin"
 
 )
+// auth middleware memvalidasi backend jwt token di setiap request
+//di pasang di route group yang memerlukan autentiksi
+func AuthMiddleware() gin.HandlerFunc{
+	return func (c *gin.Context){
+		//MENGAMBIL TOKEN DARI HEADER AUTHORIZATION
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == ""{
+			c.AbortWithStatusJSON(http.StatusUnauthorized,gin.H{
+				"success" : false,
+				"message" : "Authorization header tidak ditemukan",
+				"error_code": "MISSING_TOKEN",
+			})
+			return 
+		}
+	}
+}

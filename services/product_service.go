f2d6c624c85e199eb005d62bf2b6a16f532585ca
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 00:28:43 +0700

    membuat struc product repositories

--- services/product_service.go
+++ services/product_service.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/repositories"
+
+)
+
+type ProductService struct {
+	ProductRepo *repositories.ProductRepository
+}

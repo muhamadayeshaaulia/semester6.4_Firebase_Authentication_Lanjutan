9373b8e585aacc155dad12209e3290469cd92a5a
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 01:52:02 +0700

    membuat import untuk keperluan di handler produk

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -1,1 +1,11 @@
 package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/services"
+)

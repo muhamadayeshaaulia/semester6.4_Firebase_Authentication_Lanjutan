df4253ab336d7ffaf7dc7d8ae714fb0c245cf0c0
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 00:36:14 +0700

    menambahkan sintaks untuk mereturn func nya

--- services/product_service.go
+++ services/product_service.go
@@ -15,4 +15,5 @@ func NewProductService() *	ProductService{
 func (s *ProductService) GetAll(page, limit int, category string)([]models.Product, int64,error){
 	if page <= 0 { page  = 1}
 	if limit <= 0 || limit> 100 {limit =10}
+	return s.ProductRepo.FindAll(page,limit,category)
 }

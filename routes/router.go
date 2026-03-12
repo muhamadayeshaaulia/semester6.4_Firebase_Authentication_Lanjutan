d85825298733a8e38dd43781478aad61b0f4fb6c
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 02:05:59 +0700

    membuat func untuk setup router dngan cors middleware, longger dan recovery middleware

--- routes/router.go
+++ routes/router.go
@@ -1,11 +1,24 @@
 package routes
 
 import (
-
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/muhamadayeshaaulia/gin-firebase-backend/handlers"
 	"github.com/muhamadayeshaaulia/gin-firebase-backend/middleware"
-
 )
+
+func SetupRouter() *gin.Engine {
+	// gin.Default() sudah include Logger & Recovery middleware
+	r := gin.Default()
+	// ─── CORS Middleware (izinkan request dari Flutter app) ───
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+}

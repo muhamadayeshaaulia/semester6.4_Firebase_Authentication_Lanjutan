3dd353030411f8a002fbf03d11818ee3361404d4
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 03:06:46 +0700

    memuat import untuk keperluan menambahkan produk testing ini

--- seed/seed.go
+++ seed/seed.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/config"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
+)

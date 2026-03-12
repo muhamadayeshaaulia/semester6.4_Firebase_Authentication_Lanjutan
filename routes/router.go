b0e470c5cd3d751f964defce33240a9a2831ced0
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 02:02:37 +0700

    membuat import untuk router

--- routes/router.go
+++ routes/router.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/models"
+	"github.com/muhamadayeshaaulia/gin-firebase-backend/services"
+)

2c4e6b88a9e354b34e0af05fe9da257ce82562b3
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 00:49:07 +0700

    membuat untuk mengupdate product

--- services/product_service.go
+++ services/product_service.go
@@ -9,26 +9,60 @@ import (
 type ProductService struct {
 	ProductRepo *repositories.ProductRepository
 }
-func NewProductService() *	ProductService{
+
+func NewProductService() *ProductService {
 	return &ProductService{ProductRepo: repositories.NewProductRepository()}
 }
-func (s *ProductService) GetAll(page, limit int, category string)([]models.Product, int64,error){
-	if page <= 0 { page  = 1}
-	if limit <= 0 || limit> 100 {limit =10}
-	return s.ProductRepo.FindAll(page,limit,category)
+func (s *ProductService) GetAll(page, limit int, category string) ([]models.Product, int64, error) {
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 0 || limit > 100 {
+		limit = 10
+	}
+	return s.ProductRepo.FindAll(page, limit, category)
 }
-func (s *ProductService) GetByID (id uint)(*models.Product, error){
+func (s *ProductService) GetByID(id uint) (*models.Product, error) {
 	return s.ProductRepo.FindAll(id)
 }
-func (s *ProductService) Create(req *models.CreateProductRequest)(*models.Product, error){
+func (s *ProductService) Create(req *models.CreateProductRequest) (*models.Product, error) {
 	product := &models.Product{
-		Name: req.Name,
+		Name:        req.Name,
 		Description: req.Description,
-		Price: req.Price,
-		Stock: req.Stock,
-		Category: req.Category,
-		ImageURL: req.ImageURL,
+		Price:       req.Price,
+		Stock:       req.Stock,
+		Category:    req.Category,
+		ImageURL:    req.ImageURL,
 	}
 	err := s.ProductRepo.Create(product)
 	return product, err
 }
+func (s *ProductService) Update(id uint, req *models.UpdateProductRequest) (*models.Product, error) {
+	product, err := s.ProductRepo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+	//update hanya field yang di kirim (pointer nil tidak di update)
+	if req.Name != nil {
+		product.Name = *req.Name
+	}
+	if req.Description != nil {
+		product.Description = *req.Description
+	}
+	if req.Price != nil {
+		product.Price = *req.Price
+	}
+	if req.Stock != nil {
+		product.Stock = *req.Stock
+	}
+	if req.Category != nil {
+		product.Category = *req.Category
+	}
+	if req.ImageURL != nil {
+		product.ImageURL = *req.ImageURL
+	}
+
+	err = s.ProductRepo.Update(product)
+	return product,err
+}
+

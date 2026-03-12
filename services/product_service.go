82cbd2c36926cdab8811d9f8ad868b7b23b2aa28
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 00:35:04 +0700

    mengambil semua yang ada di page dan limit

--- services/product_service.go
+++ services/product_service.go
@@ -12,3 +12,7 @@ type ProductService struct {
 func NewProductService() *	ProductService{
 	return &ProductService{ProductRepo: repositories.NewProductRepository()}
 }
+func (s *ProductService) GetAll(page, limit int, category string)([]models.Product, int64,error){
+	if page <= 0 { page  = 1}
+	if limit <= 0 || limit> 100 {limit =10}
+}
